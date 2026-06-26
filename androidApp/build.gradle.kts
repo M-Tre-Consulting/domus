02e328cb0212c90e30858855aa6e9580fd9ebb2f
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-06-26 22:48:34 +0200

    build: update version number v0.5.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ configure<ApplicationExtension> {
         applicationId = "dev.domus.android"
         minSdk = 31
         targetSdk = 37
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     buildFeatures {
