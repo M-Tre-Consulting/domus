a2294636813e454faa5980f41494abff3bc7cb88
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-08-18 02:39:52 +0200

    build: bump app version number to v1.0.3 and version code (Android) to 14

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,8 +27,8 @@ configure<ApplicationExtension> {
         applicationId = "dev.domus.android"
         minSdk = 31
         targetSdk = 37
-        versionCode = 13
-        versionName = "1.0.2"
+        versionCode = 14
+        versionName = "1.0.3"
     }
 
     buildFeatures {
